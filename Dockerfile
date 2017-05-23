dd25feb2323e3505a955ac4267029c0de87d3960
Johannes Friedrich <j.friedrich@columbia.edu>
2017-05-23 01:02:37 -0400

    add removal of worst artefacts as manual preprocessing step.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:2
 
-ADD my_cleaned-up_spikefinder_submission.py /
+ADD my_spikefinder.py /
 
 RUN pip install -U numpy==1.11.0 \
 				pandas \
@@ -13,4 +13,4 @@ RUN pip install -U numpy==1.11.0 \
 RUN git clone https://github.com/j-friedrich/OASIS.git && cd OASIS && python setup.py build_ext -i
 RUN git clone https://github.com/codeneuro/spikefinder-python.git
 
-CMD [ "python", "./my_cleaned-up_spikefinder_submission.py" ]
+CMD [ "python", "./my_spikefinder.py", "5" ]
--- my_spikefinder.py
+++ my_spikefinder.py
@@ -1,13 +1,13 @@
 import numpy as np
+import scipy
 import pandas as pd
 import matplotlib.pyplot as plt
 import requests
 import zipfile
 import StringIO
-import scipy
 import keras
 import keras.backend as K
-from sys import path
+from sys import path, argv
 
 path.append('spikefinder-python/')
 path.append('OASIS/')
@@ -17,32 +17,63 @@ from functions import estimate_parameters
 
 
 # grab and unzip data
-for foo in ('train', 'test'):
-    r = requests.get('https://s3.amazonaws.com/neuro.datasets/challenges/spikefinder/' +
-                     'spikefinder.' + foo + '.zip', stream=True)
-    z = zipfile.ZipFile(StringIO.StringIO(r.content))
-    z.extractall()
+def grab_data():
+    for foo in ('train', 'test'):
+        r = requests.get('https://s3.amazonaws.com/neuro.datasets/challenges/spikefinder/' +
+                         'spikefinder.' + foo + '.zip', stream=True)
+        z = zipfile.ZipFile(StringIO.StringIO(r.content))
+        z.extractall()
 
 
-# parameters for each dataset obtained by Bayesian optimization / parameter sweep
-# [decay(kernel), rise(kernel), percentile(background), window_length(background),
-# shift(sparsity), noise_multiplier(sparsity)]
-params = np.array([[0.992799, 0.042201, 26, 5000, 45.5894, -0.564023],
-                   [0.991437, 0.295501, 20, 5000, 12.7123, -0.0696729],
-                   [0.996053, 0.168118, 20, 5000, 8.72845, -0.441365],
-                   [0.985170, 0.013046, 20, 5000, 13.8540, 0.0334399],
-                   [0.994729, 0.886275, 21, 5000, 22.4001, -0.550495],
-                   [0.988732, 0.584366, 28, 4000, 4.15617, -0.238822],
-                   [0.967307, 0.474290, 29, 4000, 0.07804, 0.222812],
-                   [0.992499, 0.795482, 23, 5000, 20.6628, -0.232531],
-                   [0.993040, 0.141836, 22, 4000, 41.1818, 0.00033394],
-                   [0.975445, 0.531893, 27, 4000, 45.6739, -0.352258]])
+# definitions for artefact corrections as manual preprocessing step
+def butter_highpass(cutoff, fs, order):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = scipy.signal.butter(order, normal_cutoff, btype='high', analog=False)
+    return b, a
+
+
+def butter_highpass_filter(data, cutoff=1., fs=100, order=5):
+    # cutoff and fs in Hz
+    b, a = butter_highpass(cutoff, fs, order=order)
+    y = scipy.signal.filtfilt(b, a, data)
+    return y
+
+
+def correct_artefacts(dataset):
+    Y = np.array(pd.read_csv('spikefinder.train/' + str(dataset) + '.train.calcium.csv')).T
+    if dataset == 1:
+        Y[2, :900] += 2.5 / 900 * np.arange(-900, 0)
+    elif dataset == 2:
+        Y[7, :1960] = butter_highpass_filter(Y[7])[:1960]
+        Y[12, 4870:6500] = butter_highpass_filter(Y[12])[4870:6500]
+        Y[13, :1200] = .6 + butter_highpass_filter(Y[13])[:1200]
+    elif dataset == 3:
+        Y[5, :1500] -= 2.5 * .997**np.arange(0, 1500)
+    elif dataset == 7:
+        Y[11, 13000:] = np.nan
+        Y[15, 19000:] = np.nan
+    elif dataset == 8:
+        Y[2, :1400] = .6 + butter_highpass_filter(Y[2], cutoff=2.)[:1400]
+        Y[14, :500] -= .7 * .993**np.arange(0, 500)
+    elif dataset == 9:
+        Y[3, :4000] += 3.1 / 4000 * np.arange(-4000, 0)
+        Y[4, :4000] += 3.1 / 4000 * np.arange(-4000, 0)
+        Y[7, :2900] += 3.7 / 2900 * np.arange(-2900, 0)
+        Y[8, :3700] += 2.3 / 3700 * np.arange(-3700, 0)
+        Y[18, :1000] = Y[18, 1000:2000]
+    elif dataset == 10:
+        Y[15, 17200:17300] = .75
+    return Y
+
 
 # run OASIS for dataset with provided (uglily dataset dependent) parameters
-def runOASIS(dataset, d, r, perc, window, lam_sn, mu, train=True):
-    foo = 'train' if train else 'test'
-    Y = np.array(pd.read_csv('spikefinder.' + foo + '/' +
-                             str(dataset) + '.' + foo + '.calcium.csv')).T
+def runOASIS(dataset, d, r, perc, window, lam_sn, mu, artefacts=True, train=True):
+    if train:
+        Y = correct_artefacts(dataset) if artefacts else \
+            np.array(pd.read_csv('spikefinder.train/' + str(dataset) + '.train.calcium.csv')).T
+    else:
+        Y = np.array(pd.read_csv('spikefinder.test/' + str(dataset) + '.test.calcium.csv')).T
     prep = [y[~np.isnan(y)] - scipy.ndimage.filters.percentile_filter(
         y[~np.isnan(y)], perc, window) for y in Y]
     S = np.nan * np.zeros_like(Y)
@@ -77,7 +108,21 @@ def make_decoder(w, optimizer='adadelta'):
     return decoder
 
 
+# run pipeline consisting of OASIS, regression to get init kernel, refine kernel using keras
 def run(dataset=1, tau=30, b=False, verbose=0, optimizer='adadelta'):
+    # parameters for each dataset obtained by Bayesian optimization / parameter sweep
+    # [decay(kernel), rise(kernel), percentile(background), window_length(background),
+    # shift(sparsity), noise_multiplier(sparsity)]
+    params = np.array([[0.992799, 0.0422006, 26, 5000, 45.5894, -0.564023],
+                       [0.991437, 0.295501, 20, 5000, 12.7123, -0.0696729],
+                       [0.996053, 0.168118, 20, 5000, 8.72845, -0.441365],
+                       [0.98517, 0.0130467, 20, 5000, 13.854, 0.0334399],
+                       [0.994729, 0.886275, 21, 5000, 22.4001, -0.550495],
+                       [0.988732, 0.584366, 28, 4000, 4.15617, -0.238822],
+                       [0.967307, 0.47429, 29, 4000, 0.0780378, 0.222812],
+                       [0.992499, 0.795482, 23, 5000, 20.6628, -0.232531],
+                       [0.99304, 0.141836, 22, 4000, 41.1818, 0.000333936],
+                       [0.975445, 0.531893, 27, 4000, 45.6739, -0.352258]])
     trueS = np.array(pd.read_csv('spikefinder.train/' + str(dataset) + '.train.spikes.csv')).T
     # RUN OASIS
     infS = runOASIS(dataset, *params[dataset - 1])
@@ -103,6 +148,7 @@ def run(dataset=1, tau=30, b=False, verbose=0, optimizer='adadelta'):
                           for i, s in enumerate(infS)]), 0, np.inf)
     cor1 = np.mean(np.nan_to_num(score(pd.DataFrame(S.T), pd.DataFrame(trueS.T))))
     # REFINE postprocessing kernel by optimizing for correlation instead of least squares
+    # (hardly helps)
     decoder = make_decoder(km[::-1], optimizer)  # initialize weights with least-squares solution
     history = decoder.fit(np.nan_to_num(infS[..., None]), np.nan_to_num(trueS[..., None]),
                           batch_size=len(trueS), epochs=100, verbose=verbose)
@@ -132,11 +178,13 @@ def run(dataset=1, tau=30, b=False, verbose=0, optimizer='adadelta'):
                                      str(dataset) + '.test.predict.csv', index=False)
 
 
-# run for each dataset
-cor = []
-for d in range(1, 11):
-    print 'Dataset ', d
-    run(d)
-    sc = score(pd.read_csv('spikefinder.train/' + str(d) + '.train.spikes.csv'),
-               pd.read_csv('spikefinder.train/' + str(d) + '.train.predict.csv'))
-    print np.mean(sc), np.median(sc)
+# run for each dataset or the one provided as argv
+if __name__ == "__main__":
+    grab_data()
+    datasets = range(1, 11) if len(argv) == 1 else [int(argv[1])]
+    for d in datasets:
+        print 'Dataset ', d
+        run(d)
+        sc = score(pd.read_csv('spikefinder.train/' + str(d) + '.train.spikes.csv'),
+                   pd.read_csv('spikefinder.train/' + str(d) + '.train.predict.csv'))
+        print np.mean(sc), np.median(sc)
